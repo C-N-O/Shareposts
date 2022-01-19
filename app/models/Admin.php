5f5b630375378184e0201cad0cc945326fbd70c6
Clifford Okpai <meetmancliff@yahoo.com>
2022-01-19 12:39:35 -0600

    Second commit to new branch

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -32,4 +32,25 @@ class Admin{
         }
     }
 
+    public function updatePassword($admin){
+        $this->db->query("UPDATE admins SET password = :passwordParameter WHERE id = :idParameter");
+
+        //bind values
+        $this->db->bind(':idParameter', $admin['id']);
+        $this->db->bind(':passwordParameter', $admin['new_password']);
+
+        //execute
+        if($this->db->execute())
+            return true;
+        else
+            return false;
+
+    }
+
+    public function getAdminByID($id){
+        $this->db->query("SELECT * FROM admins WHERE id=:id");
+        $this->db->bind(':id', $id);
+        return $this->db->single();
+    }
+
 }
